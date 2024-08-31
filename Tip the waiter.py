40b07a227fbe16cd4e2264676214a4f70f5468d7
Junaira <advshakerullah@gmail.com>
2024-08-31 17:34:03 +0600

    hello

--- Tip the waiter.py
+++ Tip the waiter.py
@@ -1,3 +1,14 @@
 print("\033")
 
-print("Hello world")
+def tipcalc(b , tipPerc):
+    tp = b * (0.01 * tipPerc)
+    return tp
+
+bill = int(input("Enter the bill: "))
+tip = int(input("Enter the tip Percentage: "))
+
+total = bill + tipcalc(bill , tip)
+
+print(f"Bill = {bill}")
+print(f"tip Percentage: {tip}%")
+print(f"Total bill = {total}")
